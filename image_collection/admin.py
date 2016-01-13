fabe7353c5d3db6a15c530e93d83db1cfd2dd68b
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2016-01-13 11:45:32 +0100

    Added admins

--- image_collection/admin.py
+++ image_collection/admin.py
@@ -1,11 +1,28 @@
 """Admin classes for the image_collection app."""
-# from django.contrib import admin
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
-# from . import models
+from . import models
 
 
-# class YourModelAdmin(admin.ModelAdmin):
-#    list_display = ['some', 'fields', ]
-#    search_fields = ['some', 'fieds', ]
+class ImageCollectionAdmin(admin.ModelAdmin):
+    """Custom admin for the ``ImageCollection`` model."""
+    list_display = ('identifier', 'name')
 
-# admin.site.register(models.YourModel, YourModelAdmin)
+
+class ImageSlideAdmin(admin.ModelAdmin):
+    """Custom admin for the ``ImageSlide`` model."""
+    list_display = ('collection', 'get_headline', 'get_caption', 'image')
+
+    def get_caption(self, obj):
+        return obj.caption
+    get_caption.short_description = _('description')
+
+    def get_headline(self, obj):
+        return obj.caption_headline
+    get_headline.short_description = _('headline')
+
+
+admin.site.register(models.ImageSlide, ImageSlideAdmin)
+admin.site.register(models.ImageCollection, ImageCollectionAdmin)
+
